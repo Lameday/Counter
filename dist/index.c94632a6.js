ea9a1c28c24aa16475a8d6d74070db162b9a6d71
lameday <thelameday@gmail.com>
2021-05-11 12:15:12 +0200

    gathering data and calculating difference

--- dist/index.c94632a6.js
+++ dist/index.c94632a6.js
@@ -442,7 +442,56 @@ id) /*: string*/
 }
 
 },{}],"38Dgh":[function(require,module,exports) {
+//Variables
+const inputName = document.getElementById('name-input')
+const inputDate = document.getElementById('date-input')
+const button = document.getElementById("add-button")
 
+
+
+//2021-05-04T12:02
+//Functions
+const formatMsToTimer = (ms) => {
+    let s = ms/1000
+    let hours = Math.floor(s / 3600)
+    let restOfMinutes = s % 3600
+    let minutes = Math.floor(restOfMinutes/60)
+    let restOfSeconds = restOfMinutes % 60
+    let seconds = Math.floor(restOfSeconds)
+    return hours + ":" + minutes + ":" + seconds;
+}
+
+const getTime = (futureDate) => {
+    let currentDate = new Date();
+    let time = currentDate.getTime()
+    let futureTime = Date.parse(futureDate)
+    let difference = (futureTime - time) 
+    return(formatMsToTimer(difference))
+}   
+
+
+const send = () => {
+    console.log('test')
+}
+
+//Events
+inputName.addEventListener ('keypress',function(event) {
+    if(event.keyCode === 13) {
+        if(inputName.value.length > 0 && inputDate.value.length > 1) {
+
+        } else {
+            alert('Please add name and date')
+        }
+    }
+})
+
+button.addEventListener ("click",function() {
+    if(inputName.value.length > 0 && inputDate.value.length > 1) { 
+        addNewCard(getTime(inputDate.value))
+    } else {
+        alert('Please add name and date')
+    }
+})
 },{}]},["5zraM","38Dgh"], "38Dgh", "parcelRequireb238")
 
 //# sourceMappingURL=index.c94632a6.js.map
